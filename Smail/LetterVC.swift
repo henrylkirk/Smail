2ab58f443c7ddfb8abd3e6be0b8dfdc052d687f6
henrylkirk <henry@the-kirks.com>
2017-12-19 09:50:00 -0500

    Fixed issue where toolbar was being brought in front of letter

--- Smail/LetterVC.swift
+++ Smail/LetterVC.swift
@@ -64,26 +64,4 @@ class LetterVC: UIViewController {
         })
         sealed = true
     }
-    
-    // Animate the letter to come out of envelope
-    func unsealEnvelope(){
-//        self.letterView.translatesAutoresizingMaskIntoConstraints = true // to change position despite autolayout
-//        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
-//            // Move letter up
-//            self.letterView!.frame.origin.y += -250
-//        }, completion: { (success:Bool) in
-//            // Bring envelope image to front
-//            self.rootView.bringSubview(toFront: self.envelopeBackTop)
-//            self.rootView.bringSubview(toFront: self.toolbar)
-//            // Move letter into envelope
-//            UIView.animate(withDuration: 1.0, animations: {
-//                self.letterView!.frame.origin.y += self.letterView.frame.height + 60
-//            }, completion: { (success:Bool) in
-//                // Show EnvelopeVC
-//                self.performSegue(withIdentifier: "showEnvelope", sender: nil)
-//            })
-//        })
-//        sealed = false
-    }
-    
 }
